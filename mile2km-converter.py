b76e96a2eae7453784b25bd9a429be7beb833360
astratow <davno_pora@mail.com>
2022-01-10 21:58:43 +0000

    g

--- mile2km-converter.py
+++ mile2km-converter.py
@@ -1,4 +1,6 @@
 # 1 mile = 1.60934 km
 miles = float(input ('How many miles would you like to convert: '))
 kilometers = miles * 1.60934
-print(f'{miles} is {kilometers} kilometers.')
+print(f'{miles} miles is {kilometers} kilometers.')
+
+print("{} miles is {} kilometers".format(miles, kilometers))
