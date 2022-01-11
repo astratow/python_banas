9b793f81f82db388b274c9aabba08a09b471deec
astratow <davno_pora@mail.com>
2022-01-11 21:26:46 +0000

    added downloaded material, math-functions.py created

---
+++ math-functions.py
@@ -0,0 +1,21 @@
+import math
+# rounds up
+print("ceil(4.4) =", math.ceil(4.4))
+#rounds down
+print("floor(4.4) =", math.floor(4.4))
+#absolute value
+print("fabs(-4.4) =", math.fabs(-4.4))
+#factorial (silnia)
+print("factorial(-4) =", math.factorial(4))
+#return reminder of division
+print("fmod(5,4) =", math.fmod(5,4))
+#receive a float and returns an int
+print("trunc(4.2) =", math.trunc(4.2))
+#power
+print("pow(2,2)) =", math.pow(2,2))
+#square root (pierwiastek kwadratowy
+print("sqrt(4) =", math.sqrt(4))
+#special values
+{}
+print("math.e =", math.e)
+print("matg.pi =", math.pi)
