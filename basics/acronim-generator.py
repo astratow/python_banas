9b82ef5ac9aba62e395d12392f888955dc9924f3
astratow <davno_pora@mail.com>
2022-01-31 05:51:39 +0000

    acronim generator added

--- basics/acronim-generator.py
+++ basics/acronim-generator.py
@@ -1,3 +1,7 @@
 #acronim generator
 acronim = input("Please enter the words you need acronim:\n")
-acronim =
+acronim = acronim.upper()
+list_of_words = acronim.split()
+for word in list_of_words:
+    print(word[0], end="")
+print()
